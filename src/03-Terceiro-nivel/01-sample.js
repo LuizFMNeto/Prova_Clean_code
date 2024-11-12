4838524054d9bbae89d54f3f69c245fb42ff666a
Ian Marcelino <iandevcontato@gmail.com>
2024-11-12 13:14:38 -0300

    Resolução 01 e 02 (100% Correto)

--- src/03-Terceiro-nivel/01-sample.js
+++ src/03-Terceiro-nivel/01-sample.js
@@ -1,6 +1,6 @@
 async function buscarEDetalharNave() {
     try {
-        const resposta = await fetch('https://swapi.dev/api/starships/9/');
+        const resposta = await fetch("https://swapi.dev/api/starships/9/");
         const nave = await resposta.json();
 
         const detalhesNave = `
@@ -11,16 +11,16 @@ async function buscarEDetalharNave() {
 
         console.log("Detalhes da Nave:");
         console.log(detalhesNave);
-
+        const tamanhoparametro = 100;
         const tamanhoTripulacao = parseInt(nave.crew);
-        if (tamanhoTripulacao > 100) {
-            console.log('Esta é uma nave grande.');
+        if (tamanhoTripulacao > tamanhoparametro) {
+            console.log("Esta é uma nave grande.");
         } else {
-            console.log('Esta é uma nave pequena.');
+            console.log("Esta é uma nave pequena.");
         }
 
     } catch (erro) {
-        console.error('Erro ao buscar detalhes da nave:', erro);
+        console.error("Erro ao buscar detalhes da nave:", erro);
     }
 }
 
